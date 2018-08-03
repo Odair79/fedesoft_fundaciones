8b8a061a5f915cbe6b087dc4ba6c50f740faba3a
andres2050h1 <andres2050h@gmail.com>
2018-08-02 21:50:11 -0500

    Ric1

--- servidorFundaciones/models/estudiante.js
+++ servidorFundaciones/models/estudiante.js
@@ -1,6 +1,25 @@
 
 var mongoose= require('mongoose');
 var Esquema=mongoose.Schema;
+var commentSchema = new Esquema({
+    rating:  {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment:  {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Usuarios'
+    }
+}, {
+    timestamps: true
+});
+
 
 var esquemaEstudiante= new Esquema({
     nombre:{
@@ -10,7 +29,7 @@ var esquemaEstudiante= new Esquema({
     apellido:{
         type:String,
         required:true
-    }
+    }, comentarios:[commentSchema]
 });
 
 module.exports= mongoose.model('Estudiante',esquemaEstudiante);
